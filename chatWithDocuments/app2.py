17679604f8d6ea1a146e7e1fc9c64323e08e4241
cdas123 <009chandrika@gmail.com>
2024-09-20 10:19:47 +0200

    Update app2.py

--- chatWithDocuments/app2.py
+++ chatWithDocuments/app2.py
@@ -15,56 +15,11 @@ from htmlTemplate import css, bot_template, user_template
 from utils.document_content_generator import get_api_response_content
 
 
-
-#def get_text_chunks(text):
- #   text_splitter = CharacterTextSplitter(
-  #      separator="\n",
-   #     chunk_size=1000,
-    ##   length_function=len
-   # )
-   # chunks = text_splitter.split_text(text)
-  #  return chunks
-
-
-#def get_vectorstore(text_chunks):
- #   embeddings = OpenAIEmbeddings()
-  #  # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
-  #  vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
-  #  return vectorstore
-
-
-#def get_conversation_chain(vectorstore):
- #   llm = ChatOpenAI()
-    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
-
-  #  memory = ConversationBufferMemory(
-  #      memory_key='chat_history', return_messages=True)
-  #  conversation_chain = ConversationalRetrievalChain.from_llm(
-  #      llm=llm,
-  #      retriever=vectorstore.as_retriever(),
-   #     memory=memory
-   # )
-   # return conversation_chain
-
 def conversation_handler(user_question):
     # Logic to handle conversation
     # For example, you might process the user input and generate a response
     response = {"chat_history": user_question}
     return response
-
-#def handle_userinput(user_question):
-#    print("hello")
-#    print(st.session_state.get('conversation', 0))
-#    response = st.session_state.conversation({'question': user_question})
- #   st.session_state.chat_history = response['chat_history']
-#
-#    for i, message in enumerate(st.session_state.chat_history):
-#        if i % 2 == 0:
-#            st.write(user_template.replace(
-#                "{{MSG}}", message.content), unsafe_allow_html=True)
-#        else:
-#            st.write(bot_template.replace(
- #               "{{MSG}}", message.content), unsafe_allow_html=True)
 
 
 def main():
